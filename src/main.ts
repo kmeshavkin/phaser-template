165c77a3cff26c65a58778d2e618b113b06fe43b
Kostyan1996 <meshavkin1996@gmail.com>
2020-02-02 03:38:39 +0300

    Base game template

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import Phaser from 'phaser';
+
+window.addEventListener('load', () => {
+  const game = new Phaser.Game();
+});
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        include: path.join(__dirname, '../src')
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"]
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin()]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new CleanWebpackPlugin(),
+  ]
+};
