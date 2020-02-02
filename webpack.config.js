7fc83d918d45c834352cc4b0f4375cd3962cc4b3
Kostyan1996 <meshavkin1996@gmail.com>
2020-02-02 04:03:37 +0300

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        include: path.join(__dirname, '../src')
+        include: path.join(__dirname, 'src')
       },
     ],
   },
